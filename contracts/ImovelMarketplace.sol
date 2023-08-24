369a6fac34d2c457dd5ab6a32728d1538144c026
Pedro Magalhães <pedro@scrumlaunch.com>
2023-08-24 17:41:16 -0300

    added explanations

--- contracts/ImovelMarketplace.sol
+++ contracts/ImovelMarketplace.sol
@@ -2,24 +2,31 @@
 pragma solidity ^0.8.0;
 
 contract ImovelMarketplace {
+    
+    // Estrutura para representar um Imóvel no marketplace.
     struct Imovel {
-        address payable owner;  // Modifique aqui
-        uint256 preco;
-        bool isForSale;
-        uint256 aluguelPorDia;
-        uint256 alugadoAte;
+        address payable owner;        // Proprietário do imóvel.
+        uint256 preco;                // Preço do imóvel para venda.
+        bool isForSale;               // Status de venda: verdadeiro se estiver à venda, falso caso contrário.
+        uint256 aluguelPorDia;        // Preço do aluguel por dia.
+        uint256 alugadoAte;           // Data de término do aluguel atual.
     }
 
+    // Mapeia cada ID de imóvel para um Imovel. 
     mapping(uint256 => Imovel) public imoveis;
+
+    // ID para o próximo imóvel a ser listado.
     uint256 public nextImovelId = 0;
 
+    // Eventos para logar ações importantes no contrato.
     event ImovelListado(uint256 id, uint256 preco);
     event ImovelComprado(uint256 id, address novoOwner);
     event ImovelAlugado(uint256 id, address locatario, uint256 duracao);
 
+    // Lista um novo imóvel para venda no marketplace.
     function listarImovel(uint256 preco, uint256 aluguelPorDia) public {
         imoveis[nextImovelId] = Imovel({
-            owner: payable(msg.sender),  // E aqui
+            owner: payable(msg.sender),
             preco: preco,
             isForSale: true,
             aluguelPorDia: aluguelPorDia,
@@ -30,6 +37,7 @@ contract ImovelMarketplace {
         nextImovelId++;
     } 
 
+    // Permite a compra de um imóvel listado.
     function comprarImovel(uint256 id) public payable {
         require(id < nextImovelId, "Imovel nao encontrado");
         require(imoveis[id].isForSale, "Imovel nao esta a venda");
@@ -42,6 +50,7 @@ contract ImovelMarketplace {
         emit ImovelComprado(id, msg.sender);
     }
 
+    // Permite o aluguel de um imóvel.
     function alugarImovel(uint256 id, uint256 dias) public payable {
         require(id < nextImovelId, "Imovel nao encontrado.");
         require(block.timestamp > imoveis[id].alugadoAte, "Imovel ja esta alugado.");
@@ -53,17 +62,20 @@ contract ImovelMarketplace {
         emit ImovelAlugado(id, msg.sender, dias);
     }
 
+    // Permite que o proprietário de um imóvel altere o preço de venda.
     function alterarPreco(uint256 id, uint256 novoPreco) public {
         require(imoveis[id].owner == msg.sender, "Somente o proprietario pode alterar o preco.");
         imoveis[id].preco = novoPreco;
         imoveis[id].isForSale = true;
     }
 
+    // Permite que o proprietário retire o imóvel da venda.
     function retirarDaVenda(uint256 id) public {
         require(imoveis[id].owner == msg.sender, "Somente o proprietario pode retirar da venda.");
         imoveis[id].isForSale = false;
     }
 
+    // Função para receber pagamentos no contrato.
     receive() external payable {}
 
 }
